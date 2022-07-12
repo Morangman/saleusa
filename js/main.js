c279dbe836e4384fe37f84e67d44ea2240ab1237
syniepolskyi <syniepolskyi@yahoo.com>
2022-07-12 21:23:37 +0300

    quick fix js

---
+++ js/main.js
@@ -0,0 +1,82 @@
+$(".popular-categories__slider").lightSlider({
+    controls: false,
+    item: 6,
+    loop: true,
+    responsive: [
+      {
+        breakpoint: 1919,
+        settings: {
+          item: 3,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          item: 2,
+        },
+      },
+    ],
+  });
+  $(".shoes-list-slider").lightSlider({
+    controls: false,
+    item: 6,
+    loop: true,
+    responsive: [
+    {
+        breakpoint: 1919,
+        settings: {
+          item: 4
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          item: 3
+        }
+      },
+    ]
+  });
+  $(".bags-list-slider").lightSlider({
+    controls: false,
+    item: 6,
+    loop: true,
+    responsive: [
+    {
+        breakpoint: 1919,
+        settings: {
+          item: 4
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          item: 3
+        }
+      },
+    ]
+  });
+  $(".brands-list-slider").lightSlider({
+    controls: false,
+    item: 6,
+    loop: true,
+    responsive: [
+      {
+        breakpoint: 1919,
+        settings: {
+          item: 5
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          item: 3
+        }
+      }
+    ]
+  });
+
+document
+.querySelector(".header-search-clear-button")
+.addEventListener("click", (ev) => {
+  document.querySelector(".header-search-input").value = "";
+});
